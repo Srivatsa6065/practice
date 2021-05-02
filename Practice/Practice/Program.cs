4f1e0504d56c54bcb67927bc2448ef2689b3e1f8
Srivatsa Prasad <Srivatsa.Prasad@readynetworks.com>
2021-05-02 14:44:11 +0530

    1

---
+++ Practice/Practice/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinaryToDecimal(101);
+            DecimalToBinary(1);
+        }
+
+        private static void DecimalToBinary(int @decimal)
+        {
+            Console.WriteLine($"Expected value : {Convert.ToString(@decimal, 10)}");
+
+            var binary = string.Empty;
+
+            while (@decimal > 0)
+            {
+                var reminder = @decimal % 2;
+                @decimal /= 2;
+                binary = $"{reminder}{binary}";
+            }
+
+            Console.WriteLine($"Output value : {binary}");
+        }
+
+        private static void BinaryToDecimal(int binary)
+        {
+            Console.WriteLine($"Expected value : {Convert.ToInt32(binary.ToString(), 2)}");
+
+            var @decimal = 0;
+            var weight = 1;
+
+            while (binary != 0)
+            {
+                var reminder = binary % 10;
+                @decimal += reminder * weight;
+                binary /= 10;
+                weight *= 2;
+            }
+
+            Console.WriteLine($"Output value : {@decimal}");
+        }
+    }
+}
